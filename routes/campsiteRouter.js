68002460d42bc749402b39e2b49bfb8b87e4a566
Andrew Carrigan <carrigan223@gmail.com>
2020-08-21 14:41:18 -0700

    Passport JWT

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const Campsite = require("../models/campsite");
-const { response } = require("express");//not sure if i need this double check
+const authenticate = require('../authenticate');
 
 const campsiteRouter = express.Router();
 
@@ -20,7 +20,7 @@ campsiteRouter
       .catch((err) => next(err));
   })
 
-  .post((req, res, next) => {
+  .post(authenticate.verifyUser, (req, res, next) => {
     Campsite.create(req.body)
       .then((campsite) => {
         console.log("Campsite Created", campsite);
@@ -31,12 +31,12 @@ campsiteRouter
       .catch((err) => next(err));
   })
 
-  .put((req, res) => {
+  .put(authenticate.verifyUser, (req, res) => {
     res.statusCode = 403;
     res.end("PUT operation not supported on /campsites");
   })
 
-  .delete((req, res, next) => {
+  .delete(authenticate.verifyUser, (req, res, next) => {
     Campsite.deleteMany()
       .then((response) => {
         res.statusCode = 200;
@@ -58,14 +58,14 @@ campsiteRouter
       .catch((err) => next(err));
   })
 
-  .post((req, res) => {
+  .post(authenticate.verifyUser, (req, res) => {
     res.statusCode = 403;
     res.end(
       `Post operation not supported on /campsites/${req.params.campsiteId}`
     );
   })
 
-  .put((req, res) => {
+  .put(authenticate.verifyUser, (req, res) => {
     Campsite.findByIdAndUpdate(
       req.params.campsiteId,
       {
@@ -81,7 +81,7 @@ campsiteRouter
       .catch((err) => next(err));
   })
 
-  .delete((req, res, next) => {
+  .delete(authenticate.verifyUser, (req, res, next) => {
     Campsite.findByIdAndDelete(req.params.campsiteId)
       .then((response) => {
         res.statusCode = 200;
@@ -109,7 +109,7 @@ campsiteRouter
       .catch((err) => next(err));
   })
 
-  .post((req, res, next) => {
+  .post(authenticate.verifyUser, (req, res, next) => {
     Campsite.findById(req.params.campsiteId)
       .then((campsite) => {
         if (campsite) {
@@ -131,14 +131,14 @@ campsiteRouter
       .catch((err) => next(err));
   })
 
-  .put((req, res) => {
+  .put(authenticate.verifyUser, (req, res) => {
     res.statusCode = 403;
     res.end(
       `Put operation not supported on /campsites/${req.params.campsiteId}`
     );
   })
 
-  .delete((req, res, next) => {
+  .delete(authenticate.verifyUser, (req, res, next) => {
     Campsite.findById(req.params.campsiteId)
       .then((campsite) => {
         if (campsite) {
@@ -184,7 +184,7 @@ campsiteRouter
       .catch((err) => next(err));
   })
 
-  .post((req, res) => {
+  .post(authenticate.verifyUser, (req, res) => {
     res.statusCode = 403;
     res.end(
       `POST operation not supported on /campsites/${req.params.campsiteId}/comments/${req.params.commentId}`
@@ -222,7 +222,7 @@ campsiteRouter
       .catch((err) => next(err));
   })
 
-  .delete((req, res, next) => {
+  .delete(authenticate.verifyUser, (req, res, next) => {
     Campsite.findById(req.params.campsiteId)
       .then((campsite) => {
         if (campsite && campsite.comments.id(req.params.commentId)) {
--- routes/promotionRouter.js
+++ routes/promotionRouter.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const Promotion = require("../models/promotions");
-const { response } = require("express"); //not sure if i need this double check
+const authenticate = require("../authenticate"); //not sure if i need this double check
 
 const promotionRouter = express.Router();
 
@@ -20,7 +20,7 @@ promotionRouter
       .catch((err) => next(err));
   })
 
-  .post((req, res, next) => {
+  .post(authenticate.verifyUser, (req, res, next) => {
     Promotion.create(req.body)
       .then((promotion) => {
         console.log("Promotion Created", promotion);
@@ -31,12 +31,12 @@ promotionRouter
       .catch((err) => next(err));
   })
 
-  .put((req, res) => {
+  .put(authenticate.verifyUser, (req, res) => {
     res.statusCode = 403;
     res.end("PUT operation not supported on /promotions");
   })
 
-  .delete((req, res, next) => {
+  .delete(authenticate.verifyUser, (req, res, next) => {
     Promotion.deleteMany()
       .then((response) => {
         res.statusCode = 200;
@@ -58,14 +58,14 @@ promotionRouter
       .catch((err) => next(err));
   })
 
-  .post((req, res) => {
+  .post(authenticate.verifyUser, (req, res) => {
     res.statusCode = 403;
     res.end(
       `Post operation not supported on /promotion/${req.params.promotionId}`
     );
   })
 
-  .put((req, res) => {
+  .put(authenticate.verifyUser, (req, res) => {
     Promotion.findByIdAndUpdate(
       req.params.promotionId,
       {
@@ -81,7 +81,7 @@ promotionRouter
       .catch((err) => next(err));
   })
 
-  .delete((req, res, next) => {
+  .delete(authenticate.verifyUser, (req, res, next) => {
     Promotion.findByIdAndDelete(req.params.promotionId)
       .then((response) => {
         res.statusCode = 200;
