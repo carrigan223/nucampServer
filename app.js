f9f36fde0f52a0cb7eaea388c2542a98a92e58a9
Andrew Carrigan <carrigan223@gmail.com>
2020-08-24 10:06:57 -0700

    hhtps

--- app.js
+++ app.js
@@ -29,6 +29,20 @@ connect.then(
 
 var app = express();
 
+// Secure traffic only
+app.all("*", (req, res, next) => {
+  if (req.secure) {
+    return next();
+  } else {
+    console.log(
+      `Redirecting to: https://${req.hostname}:${app.get("secPort")}${req.url}`
+    );
+    res.redirect(
+      301,
+      `https://${req.hostname}:${app.get("secPort")}${req.url}`
+    );
+  }
+});
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
@@ -37,7 +51,6 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 //app.use(cookieParser("12345-67890-98765-43210"));
-
 
 app.use(passport.initialize());
 
@@ -51,7 +64,6 @@ app.use(passport.initialize());
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-
 
 app.use(express.static(path.join(__dirname, "public")));
 
