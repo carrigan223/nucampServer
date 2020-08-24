e3eb731536b2f5a64c26bc25ec2f7653b765bc0c
Andrew Carrigan <carrigan223@gmail.com>
2020-08-24 10:42:06 -0700

    File Upload

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var usersRouter = require("./routes/users");
 const campsiteRouter = require("./routes/campsiteRouter");
 const promotionRouter = require("./routes/promotionRouter");
 const partnerRouter = require("./routes/partnerRouter");
+const uploadRouter = require('./routes/uploadRouter');
 const { Mongoose } = require("mongoose");
 
 const mongoose = require("mongoose");
@@ -70,6 +71,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/campsites", campsiteRouter);
 app.use("/partners", partnerRouter);
 app.use("/promotions", promotionRouter);
+app.use("/imageUpload", uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
