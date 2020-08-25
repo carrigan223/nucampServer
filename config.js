c5d29272b04b46db6d0f16499c1d8a39ea07a451
Andrew Carrigan <carrigan223@gmail.com>
2020-08-25 10:54:58 -0700

    oAuth

--- config.js
+++ config.js
@@ -1,4 +1,9 @@
+const fbSecret = require("./fbSecret");
 module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
-}
+  secretKey: "12345-67890-09876-54321",
+  mongoUrl: "mongodb://localhost:27017/nucampsite",
+  facebook: {
+    clientId: fbSecret.facebookId,
+    clientSecret: fbSecret.facebookSecret,
+  },
+};
